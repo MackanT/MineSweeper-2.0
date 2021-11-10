3cd4bc9b05b803b2ead53db84245fb66572f068e
Marcus Toftås <toftasm@student.chalmers.se>
2021-11-10 12:05:32 +0100

    Added efficient board generation

--- Main.py
+++ Main.py
@@ -75,6 +75,51 @@ class Minesweeper():
                 data.append(tmp)
         return data
             
+    def start_game(self, row=16, col=30, bomb=99, initial=0):
+        self.generate_board(row=row, col=col, bomb=bomb, initial=initial)
+        self.draw_board(row=row, col=col)
+
+
+    def generate_board(self, row, col, bomb, initial):
+
+        game_size = row*col
+
+        tile_indexes = [i for i in range(game_size) if i != initial]
+        bomb_indexes = np.random.choice(tile_indexes, size=bomb, replace=False)
+        
+        self.tile_values = np.zeros(game_size)
+        self.tile_values[bomb_indexes] = -1
+
+        bomb_pos = np.copy(self.tile_values)
+        bomb_pos = np.reshape(bomb_pos, (row, col))
+        bomb_counter = np.zeros(((row+2),(col+2)))
+
+        for x, y in ((0,0), (0,1), (0,2), (1,0), (1,2), (2,0), (2,1), (2,2)):
+            bomb_counter[x:x+row,y:y+col] += bomb_pos
+
+        ### Remove earlier added padding, and set bomb_locations to 0
+        bomb_counter = bomb_counter[1:-1,1:-1]
+        bomb_counter[np.where(bomb_pos == -1)] = 0
+        self.tile_values += np.reshape(np.abs(bomb_counter), game_size)        
+
+    def draw_board(self, row, col):
+
+        self.drawn_tiles = np.zeros((row, col), dtype=object)
+        self.drawn_tiles_num = np.zeros((row, col), dtype=object)
+        tmp = np.reshape(self.tile_values, (row, col))
+        w = 60
+
+        for i in range(row):
+            for j in range(col):
+                self.drawn_tiles[i,j] = self.canvas.create_rectangle(i*w, j*w, (i+1)*w, (j+1)*w, fill='#ffffff')
+                self.drawn_tiles_num[i,j] = self.canvas.create_text((i+.5)*w, (j+.5)*w, fill='#000000', text=tmp[i,j])
+                if tmp[i,j] == -1:
+                    self.canvas.itemconfig(self.drawn_tiles[i,j], fill='#3ee121')
+
+
+    def print_board(self, x, y):
+        print(np.reshape(self.tile_values, (x, y)))
+
 
     def mainloop(self):
         self.window.mainloop()
